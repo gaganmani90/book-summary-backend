28b749cd1e9907cd98cced81c847423abeedb2c0
Gagan Mani <gaganmani90@gmail.com>
2023-04-22 13:44:40 -0700

    update query

--- src/open-ai-query-controller.ts
+++ src/open-ai-query-controller.ts
@@ -1,10 +1,14 @@
 import Profile from "./models/profile";
 
 export class OpenAiQueryController {
+    /**
+     * recommend top 5 books with title, author and published date for a reader who is female 30
+     * @param profile
+     */
     public static recommendTopBooksQuery(profile: Profile): string {
-        var query = ""
-        const prefix = `recommend top ${queryParams.bookCount} books for`
-        return prefix + ` ${profile.age} , ${profile.gender} in ${queryParams.wordsLimit} words`
+        return `recommend top ${queryParams.bookCount} books with title, author and published date for
+        a reader who is ${profile.gender} ${profile.age}`
+
     }
 }
 
