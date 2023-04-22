a679d44c359809ad160aae498c594cae963e4588
Gagan Mani <gaganmani90@gmail.com>
2023-04-22 16:31:33 -0700

    50 req in 15min

--- src/routes/profile.router.ts
+++ src/routes/profile.router.ts
@@ -10,7 +10,7 @@ import rateLimit from 'express-rate-limit';
 
 export const limiter = rateLimit({
     windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 5, // limit each IP to 100 requests per windowMs
+    max: 50, // limit each IP to 100 requests per windowMs
 });
 
 
